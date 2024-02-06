9aaeb05c4d738074fc50795319e5bca6a24cae68
Gabriela França <gabifmouraroc@outlook.com>
2024-02-05 21:52:01 -0300

    limpando inputs

--- src/pages/completedForm/index.tsx
+++ src/pages/completedForm/index.tsx
@@ -3,11 +3,23 @@ import completedImage from '../../assets/icon-complete.svg'
 import { FormContent } from '../../components/FormContent/styles'
 import { useNavigate } from 'react-router-dom'
 import { Button } from '../../components/Button'
+import { useFormContext } from 'react-hook-form'
 export function CompletedOrder() {
+  const { setValue } = useFormContext()
+
+  function clearInput() {
+    setValue('name', '')
+    setValue('number', '')
+    setValue('month', '')
+    setValue('year', '')
+    setValue('cvc', '')
+  }
+
   const navigate = useNavigate()
 
   function backToForm() {
     navigate('/completeInfoCard')
+    clearInput()
   }
 
   return (
