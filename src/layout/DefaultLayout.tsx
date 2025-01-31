6f4409d082ed0e96746cd454c81022e6b24bed8e
Gabriela <gabifmouraroc@outlook.com>
2025-01-31 14:45:35 -0300

    updating zod

--- src/layout/DefaultLayout.tsx
+++ src/layout/DefaultLayout.tsx
@@ -1,36 +1,36 @@
+import { zodResolver } from '@hookform/resolvers/zod'
+import { FormProvider, useForm } from 'react-hook-form'
 import { Outlet } from 'react-router-dom'
+import * as zod from 'zod'
 import { AreaCard } from '../components/AreaCard/AreaCard'
 import { DefaultLayoutContent } from './styles'
-import { FormProvider, useForm } from 'react-hook-form'
-import * as zod from 'zod'
-import { zodResolver } from '@hookform/resolvers/zod'
 
 const currentYear = new Date().getFullYear()
 const limitYear = currentYear + 5
 
 const formCardValidationSchema = zod.object({
-  name: zod.string().min(1, 'Informe o nome do usuário'),
+  name: zod.string().min(1, 'Enter the user’s name'),
   number: zod
     .string()
-    .min(19, 'O mínimo de digitos esperados é 16')
-    .max(19, 'O máximo de dígitos ultrapassados'),
+    .min(19, 'The minimum expected digits are 16')
+    .max(19, 'The maximum number of digits exceeded'),
   month: zod
     .string()
-    .min(2, 'Informe 2 dígitos')
-    .max(2, 'Informe 2 dígitos')
+    .min(2, 'Enter 2 digits')
+    .max(2, 'Enter 2 digits')
     .refine((value) => {
       const month = parseInt(value)
       return month >= 1 && month <= 12
-    }, 'Mês inválido'),
+    }, 'Invalid month'),
   year: zod
     .string()
-    .min(4, 'Informe o ano de expiração')
-    .max(4, 'Informe 4 dígitos')
+    .min(4, 'Enter the expiration year')
+    .max(4, 'Enter 4 digits')
     .refine((value) => {
       const yearNumber = parseInt(value)
       return yearNumber >= currentYear && yearNumber <= limitYear
-    }, `Ano inválido. Deve estar entre ${currentYear} e ${limitYear}`),
-  cvc: zod.string().min(3, 'Informe o cvc').max(3, 'Informe 3 dígitos'),
+    }, `Invalid year. Must be between ${currentYear} and ${limitYear}`),
+  cvc: zod.string().min(3, 'Enter the CVC').max(3, 'Enter 3 digits'),
 })
 
 export function DefaultLayout() {
